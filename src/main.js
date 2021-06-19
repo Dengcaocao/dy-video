122fa77a437ced11082af3a3d131850086aa8f64
邓威 <1536698087@qq.com>
2021-06-19 18:00:03 +0800

    添加vuex状态管理

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 
-import axios from './api/axios.js';
-Vue.prototype.$axios = axios;
+// import axios from './api/axios.js'
+import store from './store'
+Vue.prototype.$store = store
+// Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 App.mpType = 'app'
