ad5a62f1adea2e150fa476c7c556461984a27047
邓威 <1536698087@qq.com>
2021-06-20 16:02:20 +0800

    设置导航栏背景颜色，设置内容区域高度

--- src/pages/index/city-wide.vue
+++ src/pages/index/city-wide.vue
@@ -1,35 +1,72 @@
 <template>
   <view class="city-wide">
+    <view class="status_bar">
+      <!-- 这里是状态栏 -->
+    </view>
     <!-- 自定义导航栏 -->
-    <keep-alive>
-      <Nav />
-    </keep-alive>
-    {{typeIndex}}
+    <Nav
+      ref="nav"
+      bg="#000033"
+      @sendHeight="receptionHeight"
+    />
+    <!-- content区域 -->
+    <scroll-view
+      class="scroll-Y"
+      :scroll-y="true"
+      :style="{height: scrollH}"
+    >
+    </scroll-view>
   </view>
 </template>
 <script>
+import { mapState } from 'vuex'
 import Nav from './components/nav.vue'
-import { mapState, mapMutations } from 'vuex'
 
 export default {
-  data() {
+  data () {
     return {
-      
+      scrollH: ''
     }
   },
   computed: {
     ...mapState([
-      'typeIndex'
+      'indexPage'
     ])
   },
   components: {
     Nav
+  },
+  watch: {
+    indexPage (val) {
+      if (val === '同城') {
+        this.getHeight()
+      }
+    }
+  },
+  methods: {
+    getHeight () {
+      const statusBarDom = uni.createSelectorQuery().in(this).select('.status_bar')
+      const { windowHeight } = uni.getSystemInfoSync()
+      statusBarDom.boundingClientRect().exec(statusRes => {
+        this.scrollH = windowHeight - statusRes[0].height
+      })
+    },
+    receptionHeight (height) {
+      this.scrollH = this.scrollH - height + 'px'
+    }
   }
 }
 </script>
 <style scoped>
 .city-wide {
   height: 100%;
-  background-color: chocolate;
+}
+.status_bar {
+  width: 100%;
+  height: var(--status-bar-height);
+  background-color: #000033;
+}
+.scroll-Y {
+  background-color: cornflowerblue;
 }
 </style>
