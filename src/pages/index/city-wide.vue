d844e981900777996b1826a03066b5b38bc8439a
邓威 <1536698087@qq.com>
2021-06-21 20:28:12 +0800

    同城页面

--- src/pages/index/city-wide.vue
+++ src/pages/index/city-wide.vue
@@ -15,6 +15,29 @@
       :scroll-y="true"
       :style="{height: scrollH}"
     >
+      <view class="video-list">
+        <view
+          class="video-item"
+          v-for="(item, index) in videoList"
+          :key="index"
+        >
+          <video
+            class="myVideo"
+            object-fit="fill"
+            :src="'http://172.20.10.2:1012/video/'+ item.videoSrc"
+            :show-center-play-btn="false"
+            :loop="true"
+            :controls="false"
+          >
+          </video>
+          <view class="avatar">
+            <image
+              :src="'http://172.20.10.2:1012/avatar/'+ item.avatar"
+              mode="widthFix"
+            ></image>
+          </view>
+        </view>
+      </view>
     </scroll-view>
   </view>
 </template>
@@ -25,7 +48,8 @@ import Nav from './components/nav.vue'
 export default {
   data () {
     return {
-      scrollH: ''
+      scrollH: '',
+      videoList: null
     }
   },
   computed: {
@@ -43,6 +67,9 @@ export default {
       }
     }
   },
+  created () {
+    this.getDataList()
+  },
   methods: {
     getHeight () {
       const statusBarDom = uni.createSelectorQuery().in(this).select('.status_bar')
@@ -53,6 +80,22 @@ export default {
     },
     receptionHeight (height) {
       this.scrollH = this.scrollH - height + 'px'
+    },
+    getDataList () {
+      uni.showLoading({
+        title: '',
+        mask: true
+      })
+      uni.request({
+        url: 'http://172.20.10.2:1012/user/user.json',
+        method: 'get',
+        timeout: 10000,
+        success: res => {
+          uni.hideLoading()
+          const { data } = res
+          this.videoList = data
+        }
+      })
     }
   }
 }
@@ -67,6 +110,36 @@ export default {
   background-color: #000033;
 }
 .scroll-Y {
-  background-color: cornflowerblue;
+  padding-top: 20rpx;
+  box-sizing: border-box;
+  background-color: #000033;
+}
+.video-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.video-list .video-item {
+  position: relative;
+  width: 372rpx;
+  height: 554rpx;
+  margin-bottom: 6rpx;
+}
+.video-item .myVideo {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+.video-list .avatar {
+  position: absolute;
+  left: 20rpx;
+  bottom: 20rpx;
+  width: 60rpx;
+  height: 60rpx;
+  border-radius: 50%;
+  overflow: hidden;
+}
+.avatar image {
+  width: 100%;
 }
 </style>
